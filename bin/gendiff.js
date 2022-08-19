d4f1dedb319332ccd4c41a79512f72306612c80d
Alina Abdul <alinka140901@gmail.com>
2022-08-19 16:56:02 +0300

    add arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,14 @@
 import { Command } from "../node_modules/commander/esm.mjs";
 
 const program = new Command;
+// const diff = genDiff(filepath1, filepath2);
+// console.log(diff);
+
 
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 program.parse();
