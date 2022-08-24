25f01510e449da64b52419435d56d0b8eb059ee1
Alina Abdul <alinka140901@gmail.com>
2022-08-24 11:25:41 +0300

    make getDifference

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
 import { Command } from "../node_modules/commander/esm.mjs";
+import { getDifference } from '../src/index.js'
 
 const program = new Command;
-// const diff = genDiff(filepath1, filepath2);
-// console.log(diff);
-
 
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
   .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(getDifference(filepath1, filepath2));
+  });
 program.parse();
